c96e984349ff67340e9c36fd928bdf084114884a
rydnr <github@acm-sl.org>
2024-01-17 19:16:46 +0100

    Added vim and emacs settings

--- pythoneda/realm/rydnr/rydnr.py
+++ pythoneda/realm/rydnr/rydnr.py
@@ -116,3 +116,11 @@ class Rydnr(EventListener):
         """
         Rydnr.logger().info(f"Running {type(event)}")
         await event.nix_flake.run()
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
